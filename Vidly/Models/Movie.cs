81bb3e2252cac6aee8dd2c8ede9b67003dbdc1b7
hsoin <hsoin@BOS1LP-03533>
2017-02-09 23:05:42 -0500

    Add project files.

---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    using System.Security.AccessControl;
+
+    public class Movie
+    {
+        public int MovieID { get; set; }
+        public string MovieName { get; set; }
+    }
+}

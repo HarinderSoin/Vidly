b9e812ee0476c51a1e739d93196adda7227ea3bd
hsoin <hsoin@BOS1LP-03533>
2017-02-15 13:08:06 -0500

    Vidly with customer data including Membership Name

---
+++ Vidly/Migrations/201702151753319_PopulateMembershipName.cs
@@ -0,0 +1,20 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateMembershipName : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("Update MembershipTypes set MembershipName = 'Pay As You Go' where id =1");
+            Sql("Update MembershipTypes set MembershipName = 'Monthly ' where id =2");
+            Sql("Update MembershipTypes set MembershipName = 'Quarterly' where id =3");
+            Sql("Update MembershipTypes set MembershipName = 'Yearly' where id =4");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -5,9 +5,14 @@ using System.Web;
 
 namespace Vidly.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class MembershipType
     {
         public byte Id { get; set; }
+        [Required]
+
+        public string MembershipName { get; set; }
         public short SignUpFee { get; set; }
 
         public byte DurationInMonths { get; set; }
